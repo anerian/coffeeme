c057c9d39f3d6e188170d40751ea8f2cd1115e71
Todd Fisher <taf2@glazgo.(none)>
2009-05-13 10:25:56 -0400

    better position text in coffee cup and add a 1 px shadow

--- Classes/CMTriviaView.m
+++ Classes/CMTriviaView.m
@@ -17,21 +17,25 @@
     if (self = [super initWithFrame:frame]) {
         self.userInteractionEnabled = YES;
 
-        self.contentMode = UIViewContentModeCenter;
         // Initialization code
         self.image = [UIImage imageNamed:@"trivia-bg.png"];
 
         // center the label over the coffee cup
-        CGRect rect = CGRectMake(60.0f, 130.0f, 180.0f, 130.0f);
+        CGRect rect = CGRectMake(55.0f, 90.0f, 200.0f, 190.0f);
 
         self.label = [[[UILabel alloc] initWithFrame:rect] autorelease];
         self.label.backgroundColor = [UIColor clearColor];
         self.label.textColor = [UIColor whiteColor];
-        self.label.numberOfLines = 10;
-        self.label.adjustsFontSizeToFitWidth = YES;
+        self.label.numberOfLines = 0;
+        CGSize offset;
+        offset.width = 1;
+        offset.height = 1;
+        self.label.shadowOffset = offset;
+        self.label.shadowColor = [UIColor blackColor];
+        //self.label.adjustsFontSizeToFitWidth = YES;
+        self.label.font = [UIFont systemFontOfSize:15];
         self.label.lineBreakMode = UILineBreakModeWordWrap;
-
-        [self updateTrivia];
+        [self.label setTextAlignment:UITextAlignmentCenter];
 
         [self setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
         [self addSubview:self.label];
@@ -42,15 +46,10 @@
 -(void) updateTrivia {
     NSString *str = [CMTrivia randomTrivia].fact;
     [self.label setText: str ];
-    [self.label setTextAlignment:UITextAlignmentCenter];
     [str release];
 }
 
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    NSLog(@"touches began count %d, %@", [touches count], touches);
-}
 - (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    NSLog(@"touches moved count %d, %@", [touches count], touches);
     [self updateTrivia];
 }
 
