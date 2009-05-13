af2c3635f457f0c5df5aa63729dd2096bcd3aae3
Min Kim <minho.kim@gmail.com>
2009-05-13 13:28:51 -0400

    adding settings controller

---
+++ Classes/CMSettingsController.h
@@ -0,0 +1,16 @@
+//
+//  CMSettingsController.h
+//  CoffeeMe
+//
+//  Created by min on 5/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CMSettingsController : UIViewController {
+
+}
+
+@end
