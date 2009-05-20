d2feba71ed35bc15ab5617beeec7e20fb98dd30c
Todd Fisher <taf2@glazgo.(none)>
2009-05-20 18:55:11 -0400

    allow user to filter for a store

--- Server/app/controllers/stores_controller.rb
+++ Server/app/controllers/stores_controller.rb
@@ -7,9 +7,26 @@ class StoresController < ApplicationController
     if not params[:store_type].blank?
       conditions.merge!(:store_type => params[:store_type])
     end
+    if not params[:filter].blank?
+      options.merge!(:filter => params[:filter])
+    end
     options.merge!(:conditions => conditions)
-    session[:store_index] = options
-    @stores = Store.paginate(:all, options)
+    session[:store_index] = options.clone
+    logger.debug("store_index with: #{options.inspect}")
+    filter = options.delete(:filter)
+    if filter
+      values = []
+      conditions = options.delete(:conditions).map{|k,v| values << v; "#{k}=?" }.join(' ')
+      conditions << " AND " if not conditions.blank?
+      conditions << " (street LIKE ? OR city LIKE ? OR state LIKE ? OR phone LIKE ?)"
+      4.times { values << "%#{filter}%" }
+      conditions = [conditions] + values
+      logger.debug("filters: #{conditions.inspect}")
+      options.merge!(:conditions => conditions)
+      @stores = Store.paginate(:all, options)
+    else
+      @stores = Store.paginate(:all, options)
+    end
   end
 
   def show
@@ -38,8 +55,9 @@ class StoresController < ApplicationController
     if @store.update_attributes(params[:store])
       flash[:updated_item] = params[:id]
       options = session[:store_index]
-      if options and options.key?(:page)
-        redirect_to stores_path(:page => options[:page])
+      if options and (options.key?(:page) or options.key?(:filter))
+        logger.debug("redirect with options: #{options.inspect}")
+        redirect_to stores_path(:page => options[:page], :filter => options[:filter])
       else
         redirect_to stores_path
       end
