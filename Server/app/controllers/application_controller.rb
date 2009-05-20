cda3d3a87f12abe0b81acb2250f8cf6f14644784
Todd Fisher <taf2@glazgo.(none)>
2009-05-20 19:18:15 -0400

    ready the app for deployment

--- Server/app/controllers/application_controller.rb
+++ Server/app/controllers/application_controller.rb
@@ -4,7 +4,20 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  Users = {"anerian" => "anerian-rocks"}.freeze
+  before_filter :authenticate
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  def secret
+    render :text => "Password Required!"
+  end 
+
+private
+  def authenticate
+    realm = "Coffee ME"
+    authenticate_or_request_with_http_digest(realm) do |name|
+      Users[name]
+    end
+  end 
 end
---
+++ Server/config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "coffeeme"
+set :repository,  "git@github.com:anerian/coffeeme.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/apps/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "rails1"
+role :web, "rails1"
+role :db,  "rails1", :primary => true
