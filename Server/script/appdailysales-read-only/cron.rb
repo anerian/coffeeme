3c72c625b747509e9a32da240727feb762cbb188
Todd Fisher <taf2@glazgo.(none)>
2009-06-29 15:37:29 -0400

    cd to working dir

--- Server/script/appdailysales-read-only/cron.rb
+++ Server/script/appdailysales-read-only/cron.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
 root = File.expand_path(File.dirname(__FILE__))
-system("/usr/bin/python #{File.join(root,'appdailysales.py')}")
-system("/opt/local/bin/ruby #{File.join(root,'parse.rb')}")
+system("cd #{root} && /usr/bin/python #{File.join(root,'appdailysales.py')}")
+system("cd #{root} && /opt/local/bin/ruby #{File.join(root,'parse.rb')}")
